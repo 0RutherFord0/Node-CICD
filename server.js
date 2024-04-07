6991658a802e57ee095031fcb353f8746c1316d9
Aditya Tawade <adityatwd0@gmail.com>
2024-04-07 16:18:41 +0530

    Node Server Change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 // Define a route for the API
 app.get("/api/name", (req, res) => {
   // Replace 'Your Name' with your actual name
-  const name = "Hello World";
+  const name = "My Name is Aditya Tawade";
   // Sending the name in JSON format
   res.json({ name });
 });
