2f5730c0625d3c971216c0d7d058aa31d919bb04
Aditya Tawade <adityatwd0@gmail.com>
2024-04-07 13:54:04 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Define a route for the API
+app.get("/api/name", (req, res) => {
+  // Replace 'Your Name' with your actual name
+  const name = "Aditya Tawade";
+  // Sending the name in JSON format
+  res.json({ name });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
