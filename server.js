af731dd4c26ffc29eaebe6dbc33fdaefb1a2c6d8
Aditya Tawade <adityatwd0@gmail.com>
2024-04-07 15:59:41 +0530

    Server is Running

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 // Define a route for the API
 app.get("/api/name", (req, res) => {
   // Replace 'Your Name' with your actual name
-  const name = "Adieeeeeeeeeee Tawadeeeeeeeeeeeeeeeeeee";
+  const name = "Server is Running";
   // Sending the name in JSON format
   res.json({ name });
 });
