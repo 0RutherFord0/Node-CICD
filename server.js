254e04e86f37b7559d0e1a2c18d372b4acc09a91
Aditya Tawade <36890395+0RutherFord0@users.noreply.github.com>
2024-04-08 16:52:56 +0530

    Update V6 server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const generatePassword = require("generate-password");
 const PORT = process.env.PORT || 3000;
 
 // Define a route for the API
@@ -19,20 +18,7 @@ app.get("/api/age", (req, res) => {
   res.json({ age });
 });
 
-// Define a route to generate a random password
-app.get("/api/password", (req, res) => {
-  // Generate a random password
-  const password = generatePassword.generate({
-    length: 12, // Length of the password
-    numbers: true, // Include numbers
-    symbols: true, // Include symbols
-    uppercase: true, // Include uppercase letters
-    excludeSimilarCharacters: true, // Exclude similar characters like 'i' and 'l'
-  });
 
-  // Sending the password in JSON format
-  res.json({ password });
-});
 
 // Start the server
 app.listen(PORT, () => {
