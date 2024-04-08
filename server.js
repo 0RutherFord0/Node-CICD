904bec9a53b8f059ed993a5d850a6b8c044726b7
Aditya Tawade <36890395+0RutherFord0@users.noreply.github.com>
2024-04-08 15:21:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.get("/api/name", (req, res) => {
   res.json({ name });
 });
 
+// Define a route for the API
+app.get("/api/age", (req, res) => {
+  // Replace 'Your Name' with your actual name
+  const age = "28";
+  // Sending the name in JSON format
+  res.json({ age });
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
